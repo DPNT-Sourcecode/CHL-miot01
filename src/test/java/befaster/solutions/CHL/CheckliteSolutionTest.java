f1d613b585ef52e31bb864b28f2e9743ab354c83
sbx_user1051 <sbx_user1051@169.254.175.173>
2019-11-03 13:23:08 +0000

    Sun Nov 03 13:23:08 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,4 +1,20 @@
 package befaster.solutions.CHL;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class CheckliteSolutionTest {
+    private CheckliteSolution solution;
+
+    @Before
+    public void setup() {
+        solution = new CheckliteSolution();
+    }
+
+    @Test
+    public checkQuantityForItemsTest() {
+
+    }
+
 }
+
