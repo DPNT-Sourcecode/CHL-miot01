f8d995aa6dce731be09be9fc05492b160cbbb3a5
sbx_user1051 <sbx_user1051@169.254.175.173>
2019-11-03 14:05:08 +0000

    Sun Nov 03 14:05:08 UTC 2019

--- src/main/java/befaster/solutions/CHL/model/Item.java
+++ src/main/java/befaster/solutions/CHL/model/Item.java
@@ -11,6 +11,8 @@ public class Item {
     // map of special offers, quantity - price
     private Map<Long, Integer> specialOffers;
 
+    private Long highestOfferQuantity = null;
+
     public Item(
         String sku,
         Integer defaultPrice,
@@ -22,9 +24,20 @@ public class Item {
     }
 
     public Integer getPrice(Long quantity) {
+        // we need to check how many times do we have the offer in the quantity we get
+        // so we can have an offer for 3 but the quantity is 7 so we would have 2*offer + normal price
+
         return specialOffers.getOrDefault(quantity, defaultPrice);
     }
+
+    private Long getHighestOfferQuantity() {
+        if (highestOfferQuantity == null) {
+            highestOfferQuantity = specialOffers.keySet().stream().max(Long::compareTo).orElse(1L);
+        }
+        return highestOfferQuantity;
+    }
 }
+
 
 
 
