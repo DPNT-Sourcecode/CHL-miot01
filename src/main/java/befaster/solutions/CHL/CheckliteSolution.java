c2a47568352d5390a028ff6f2c4f86454f4b421d
sbx_user1051 <sbx_user1051@169.254.175.173>
2019-11-03 13:20:08 +0000

    Sun Nov 03 13:20:08 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -3,9 +3,9 @@ package befaster.solutions.CHL;
 import befaster.runner.SolutionNotImplementedException;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CheckliteSolution {
@@ -13,13 +13,19 @@ public class CheckliteSolution {
         throw new SolutionNotImplementedException();
     }
 
-    private Map<String, Long> getQuantityPerItems(String skus) {
+    /**
+     * Gets the quantity for each item in the basket
+     * @param skus comma separated list of items
+     * @return map of item to quantity
+     */
+    public Map<String, Long> getQuantityPerItems(String skus) {
         // we assume that the sku is a comma separate list of items
         List<String> items = Arrays.asList(skus.split(","));
 
         return items.stream()
-            .collect(Collectors.groupingBy(s -> s, Collectors.joining()));
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 }
+
 
 
