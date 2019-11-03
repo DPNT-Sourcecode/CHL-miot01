7063ccc8654bfa1e9c7121815fb69ba8882fec1b
sbx_user1051 <sbx_user1051@169.254.175.173>
2019-11-03 16:13:03 +0000

    Sun Nov 03 16:13:03 UTC 2019

--- src/main/java/befaster/solutions/CHL/model/Item.java
+++ src/main/java/befaster/solutions/CHL/model/Item.java
@@ -30,33 +30,41 @@ public class Item {
         this.freeItemOffers = freeItemOffers;
     }
 
+//    public Integer getPrice(Long quantity) {
+//        // we need to check how many times do we have the offer in the quantity we get
+//        // so we can have an offer for 3 but the quantity is 7 so we would have 2*offer + normal price
+//        if (! specialOffers.isEmpty() && quantity > getHighestOfferQuantity()) {
+//            Long offerNumber = quantity/getHighestOfferQuantity();
+//            Long remainingSoloItems = quantity%getHighestOfferQuantity();
+//            return
+//                offerNumber.intValue()*specialOffers.get(getHighestOfferQuantity())
+//                + remainingSoloItems.intValue() * defaultPrice;
+//
+//        }
+//        else {
+//            return Optional
+//                .ofNullable(specialOffers.get(quantity))
+//                .orElse(quantity.intValue() * defaultPrice);
+//        }
+//    }
+
     public Integer getPrice(Long quantity) {
-        // we need to check how many times do we have the offer in the quantity we get
-        // so we can have an offer for 3 but the quantity is 7 so we would have 2*offer + normal price
-        if (! specialOffers.isEmpty() && quantity > getHighestOfferQuantity()) {
-            Long offerNumber = quantity/getHighestOfferQuantity();
-            Long remainingSoloItems = quantity%getHighestOfferQuantity();
-            return
-                offerNumber.intValue()*specialOffers.get(getHighestOfferQuantity())
-                + remainingSoloItems.intValue() * defaultPrice;
-
-        }
-        else {
-            return Optional
-                .ofNullable(specialOffers.get(quantity))
-                .orElse(quantity.intValue() * defaultPrice);
-        }
-    }
-
-    public Integer getPrice2(Long quantity) {
         Long remainingQuantity = quantity;
         Integer price = 0;
-        while (remainingQuantity > 0) {
+        while (remainingQuantity > 0 && hasOffer) {
             // get highest offer
             Long highestOfferQuantity = getHighestQuantityOffer(remainingQuantity);
+            if (highestOfferQuantity < 0) {
+                break;
+            }
             remainingQuantity -= highestOfferQuantity;
             price += specialOffers.get(highestOfferQuantity);
         }
+
+        if (remainingQuantity > 0) {
+            price += remainingQuantity.intValue() * defaultPrice;
+        }
+        return price;
     }
 
     private Long getHighestQuantityOffer(Long quantity) {
@@ -98,4 +106,5 @@ public class Item {
         return highestOfferQuantity;
     }
 }
+
 
