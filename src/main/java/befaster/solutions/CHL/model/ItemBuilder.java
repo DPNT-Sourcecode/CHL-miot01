83dc8cc4f1a4b79e65e9112217c143250a386e85
sbx_user1051 <sbx_user1051@169.254.175.173>
2019-11-03 17:22:37 +0000

    Sun Nov 03 17:22:37 UTC 2019

--- src/main/java/befaster/solutions/CHL/model/ItemBuilder.java
+++ src/main/java/befaster/solutions/CHL/model/ItemBuilder.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHL.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ItemBuilder {
@@ -7,8 +8,37 @@ public class ItemBuilder {
     // the price for one item
     private Integer defaultPrice;
     // map of special offers, quantity - price
-    private Map<Long, Integer> specialOffers;
-
+    private Map<Long, Integer> specialOffers = new HashMap<>();
     // map of free item offers, quantity -item sku
-    private Map<Long, FreeItemOffer> freeItemOffers;
+    private Map<Long, FreeItemOffer> freeItemOffers = new HashMap<>();
+
+    public ItemBuilder sku(String sku) {
+        this.sku = sku;
+        return this;
+    }
+
+    public ItemBuilder price(Integer price) {
+        this.defaultPrice = price;
+        return this;
+    }
+
+    public ItemBuilder addSpecialOffer(Long quantity, Integer price) {
+        this.specialOffers.put(quantity, price);
+        return this;
+    }
+
+    public ItemBuilder addFreeItemOffer(Long quantity, FreeItemOffer offer) {
+        this.freeItemOffers.put(quantity, offer);
+        return this;
+    }
+
+    public Item build() {
+        return new Item(
+            sku,
+            defaultPrice,
+            specialOffers,
+            freeItemOffers
+        );
+    }
 }
+
